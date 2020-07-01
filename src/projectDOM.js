29a7e0c89127fc2139d8db60f05fecf97f72dbd7
osechi3 <holypoetwrk@gmail.com>
2020-07-01 22:54:11 +0300

    Creating logic allowing to show only those todos that are in the project

--- src/projectDOM.js
+++ src/projectDOM.js
@@ -34,13 +34,36 @@ const createAProjectDOM = () => {
 
 const appendProject = (newProject) => {
 
-  // Adding to the project section
+  // Adding a new project to the project section
 
   const newProjectItem = document.createElement('li');
-  newProjectItem.textContent = newProject.name;
+  newProjectItem.setAttribute('id', newProject.name.toLowerCase());
   projectList.insertBefore(newProjectItem, projectCreationBtn);
 
-  // Adding to the input container where a user can choose a project to put a new to-do into
+  const newProjectItemBtn = document.createElement('a');
+  newProjectItemBtn.setAttribute('href', '#');
+  newProjectItemBtn.textContent = newProject.name;
+  newProjectItem.appendChild(newProjectItemBtn);
+
+  // A new project item functionality
+
+  // When clicked hide the to-dos that are not in the project
+  newProjectItemBtn.addEventListener('click', () => {
+    const todoProjectNames = document.querySelectorAll('.todo-project-name');
+    todoProjectNames.forEach((name) => {
+      console.log(name.dataset.order);
+      if (name.textContent !== newProjectItemBtn.textContent) {
+        
+        // Hiding the to-do containers that aren't in the project
+        helperFunctions().toHide(name.parentElement.parentElement);
+        
+        // Removing them from the flow of the page
+        name.parentElement.parentElement.style.cssText = 'position: absolute';
+      }
+    });
+  });
+  
+  // Adding a new project to the input container where a user can choose a project to put a new to-do into
 
   const inputProjectSelector = document.querySelector('#input-project');
   const newProjectOption = document.createElement('option');
