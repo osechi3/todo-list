4a5c7f4ef56372376e813a12d0b5d9d42a96cb34
osechi3 <holypoetwrk@gmail.com>
2020-07-07 22:51:48 +0300

    Adding placeholders into the edit box's title and description sections

--- src/todoDOM.js
+++ src/todoDOM.js
@@ -147,6 +147,7 @@ const appendATodo = (newTodo) => {
     editTitle.setAttribute('type', 'text');
     editTitle.setAttribute('name', 'edit-title');
     editTitle.setAttribute('maxlength', '45');
+    editTitle.setAttribute('placeholder', 'Title');
     editLine1.appendChild(editTitle);
 
     // Description
@@ -159,6 +160,7 @@ const appendATodo = (newTodo) => {
     editDescription.setAttribute('type', 'text');
     editDescription.setAttribute('name', 'edit-description');
     editDescription.setAttribute('maxlength', '150');
+    editDescription.setAttribute('placeholder', 'Description');
     editLine1.appendChild(editDescription);
 
     // Due date
